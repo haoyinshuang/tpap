7c5af7da996f5f9a9aa262ac5a003be2caee78dd
haoyinshuang <373660351@qq.com>
2013-12-03 10:38:06 +0800

    create index.js
    要实现弹出层对话框功能，学习中

---
+++ assets/openjs/kissy/gallery/overlay/1.0/index.js
@@ -0,0 +1,8 @@
+KISSY.add(function (S, Node, Overlay, Button) {
+    //适配代码在这
+    function SafeOverlay(frameGroup) {      
+    }
+    return SafeOverlay;
+}, {
+    requires: ['core','node', 'overlay', 'button', 'overlay/1.0/index.js']
+});
